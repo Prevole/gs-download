e25854152fa2aec53f85908e65277187ad5b60d1
Prevole <prevole@prevole.ch>
2019-02-24 12:11:25 +0100

    Fixed to match latest genius app version

--- index.js
+++ index.js
@@ -20,18 +20,20 @@ const optionDefinitions = [{
 
 const options = commandLineArgs(optionDefinitions);
 
+console.log(options);
+
 const baseUrl = 'http://' + options.host + ':' + options.port + '/files';
 
 console.log(baseUrl);
 
 function download(target, files, idx) {
-    request(baseUrl + '/' + files[idx])
+    request(baseUrl + '/files/' + files[idx].uid)
         .on('end', function() {
-            console.log('Download completed for file: ' + files[idx]);
+            console.log('Download completed for file: ' + files[idx].name);
+
             if (idx < files.length - 1) {
                 download(target, files, idx + 1);
-            }
-            else {
+            } else {
                 console.log(files.length + " files downloaded");
             }
         })
@@ -39,11 +41,16 @@ function download(target, files, idx) {
             console.log("Error occurred for file " + idx + " on " + files.length);
             console.log(error);
         })
-        .pipe(fs.createWriteStream(path.join(target, files[idx])));
+        .pipe(fs.createWriteStream(path.join(target, files[idx].name)));
 }
 
 request(baseUrl, function(error, response, body) {
-    files = JSON.parse(body);
-    console.log(files.length + " files to download");
-    download(options.target, files, 0);
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const files = JSON.parse(body);
+  console.log(files.length + " files to download");
+  download(options.target, files, 0);
 });
